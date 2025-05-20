564b2ed575740df87292f0e9ddeeee01b3c770b6
user <user@DESKTOP-2BQ3RJ3>
2025-05-19 22:39:09 -0700

    Added UI elements to main form.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            addBtn = new Button();
+            label1 = new Label();
+            productTxt = new TextBox();
             SuspendLayout();
+            // 
+            // addBtn
+            // 
+            addBtn.Location = new Point(98, 220);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(115, 67);
+            addBtn.TabIndex = 0;
+            addBtn.Text = "Add Product";
+            addBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(53, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Product Name";
+            // 
+            // productTxt
+            // 
+            productTxt.Location = new Point(154, 96);
+            productTxt.Name = "productTxt";
+            productTxt.Size = new Size(100, 23);
+            productTxt.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(318, 450);
+            Controls.Add(productTxt);
+            Controls.Add(label1);
+            Controls.Add(addBtn);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button addBtn;
+        private Label label1;
+        private TextBox productTxt;
     }
 }
